4cb7bff3a7810541c85ef29c15f0b08d99817b87
smhailey <smhailey@msn.com>
2019-06-12 16:02:21 -0600

    2 of 3 functions complete.

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-function iChooseRock() {
 
+function playerChoice(input) {
+  let playerWeapon;
+  if (input == 'rock') {
+    playerWeapon = 'Rock'
+    compare(playerWeapon)
+  }
+  else if (input == 'paper') {
+    playerWeapon = 'Paper'
+    compare(playerWeapon)
+  }
+  else if (input == 'scissors') {
+    playerWeapon = 'Scissors'
+    compare(playerWeapon)
+  }
 }
-function iChoosePaper() {
 
+function compare(playerWeapon) {
+  let cpuWeapon;
+  let index = Math.floor(Math.random() * 3)
+  if (index == 0) {
+    cpuWeapon = "Rock"
+    displayResult(playerWeapon, cpuWeapon)
+  } else if (index == 1) {
+    cpuWeapon = 'Paper'
+    displayResult(playerWeapon, cpuWeapon)
+  } else if (index == 2) {
+    cpuWeapon = 'Scissors'
+    displayResult(playerWeapon, cpuWeapon)
+  }
+  console.log("index", index)
+  console.log("player choice", playerWeapon)
+  console.log("cpu choice", cpuWeapon)
 }
-function iChooseScissors() {
+
+function displayResult(playerWeapon, cpuWeapon) {
 
 }
